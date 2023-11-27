1c3b3f83b094d8801931442cb841bd2ec176c788
akirakankambar <121007665+akirakankambar@users.noreply.github.com>
2023-11-28 00:26:47 +0800

    remove css class

--- src/components/GoogleLoginModal.vue
+++ src/components/GoogleLoginModal.vue
@@ -3,7 +3,7 @@
     <div class="modal-content">
       <h2>Login</h2>
       <!-- Google Login -->
-      <GoogleLogin :callback="callback" class="google" />
+      <GoogleLogin :callback="callback" />
       <div class="divider">OR</div>
 
       <!-- Username and Password Login -->
@@ -78,10 +78,6 @@ export default {
   gap: 2vh;
   width: 40vw;
   height: auto; /* Set a fixed width for the modal */
-}
-
-.google {
-  width: 74%;
 }
 
 .modal-content h2 {
